f594b915a7cc7f2161da53c0effe6341b51864ba
benjamesmiller <benjamesmiller@gmail.com>
2018-05-22 14:06:26 -0400

    Added babel and notes to js

--- music.js
+++ music.js
@@ -3,3 +3,11 @@ import $ from 'jquery'
 
 window.$ = $
 
+function itunesAPI(searchTerm) {
+   return `https://itunes.apple.com/search?term=${searchTerm}&entity=music&limit=25`
+}
+// When clicking the submit button, use the text in the input field
+// to pull datasets from the itunes api. Take those datasets
+// and return the Album artwork, the song title, and the Artist Name and
+// displayed below.
+
